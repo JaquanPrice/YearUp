d9c1f6aa46ee080c51e3a1929c57d3cf0b328c3f
JaquanPrice <jprice@atl.yearup.org>
2015-10-06 10:52:31 -0400

    This project is an Introduction of myself.

---
+++ Intro/src/Intro.java
@@ -0,0 +1,13 @@
+
+public class Intro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	{ 
+		System.out.println("Hello! My Name is Jaquan.");
+		System.out.println("My favorite movie is" + "The Mask");
+		System.out.println("I can play multiple instruments including the trumpet & keyboard");
+	}
+}
